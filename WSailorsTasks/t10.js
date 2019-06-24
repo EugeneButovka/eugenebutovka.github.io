1ba75a68e0a6c92a70b3f7e249b54ed3b735a2bb
EugeneButovka <eugene.butovka@gmail.com>
2019-06-24 10:21:59 +0300

    deleted map - fulfill tz

--- WSailorsTasks/t10.js
+++ WSailorsTasks/t10.js
@@ -1,65 +1,22 @@
-// This example requires the Places library. Include the libraries=places
-// parameter when you first load the API. For example:
-// <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
 function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -33.8688, lng: 151.2195},
-        zoom: 13
-    });
-    var card = document.getElementById('pac-card');
     var input = document.getElementById('pac-input');
-    var types = document.getElementById('type-selector');
-    var strictBounds = document.getElementById('strict-bounds-selector');
-
-    map.controls[google.maps.ControlPosition.TOP_RIGHT].push(card);
 
     var autocomplete = new google.maps.places.Autocomplete(input);
 
-    // Bind the map's bounds (viewport) property to the autocomplete object,
-    // so that the autocomplete requests use the current map bounds for the
-    // bounds option in the request.
-    autocomplete.bindTo('bounds', map);
-
-    // Set the data fields to return when the user selects a place.
     autocomplete.setFields(
         ['address_components', 'geometry', 'icon', 'name']);
 
-    var infowindow = new google.maps.InfoWindow();
-    var infowindowContent = document.getElementById('infowindow-content');
-    infowindow.setContent(infowindowContent);
-    var marker = new google.maps.Marker({
-        map: map,
-        anchorPoint: new google.maps.Point(0, -29)
-    });
 
     autocomplete.addListener('place_changed', function () {
-        infowindow.close();
-        marker.setVisible(false);
         var place = autocomplete.getPlace();
         if (!place.geometry) {
-            // User entered the name of a Place that was not suggested and
-            // pressed the Enter key, or the Place Details request failed.
             window.alert("No details available for input: '" + place.name + "'");
             return;
         }
 
-        // If the place has a geometry, then present it on a map.
-        if (place.geometry.viewport) {
-            map.fitBounds(place.geometry.viewport);
-        } else {
-            map.setCenter(place.geometry.location);
-            map.setZoom(17);  // Why 17? Because it looks good.
-        }
-        marker.setPosition(place.geometry.location);
-        marker.setVisible(true);
-
-        //MY CODE BEGIN
+        //MY CODE BEGIN------------------------------------------------------------------------------------
         getWeather(place.geometry.location.lat(),place.geometry.location.lng());
-        //console.log(place.geometry.location.lat(),place.geometry.location.lng());
-        //getWeather(place.geometry.location);//transfer location to outer code
-        //currLocation = place.geometry.location;
-        //MY CODE END
+        //MY CODE END--------------------------------------------------------------------------------------
 
         var address = '';
         if (place.address_components) {
@@ -69,15 +26,8 @@ function initMap() {
                 (place.address_components[2] && place.address_components[2].short_name || '')
             ].join(' ');
         }
-
-        infowindowContent.children['place-icon'].src = place.icon;
-        infowindowContent.children['place-name'].textContent = place.name;
-        infowindowContent.children['place-address'].textContent = address;
-        infowindow.open(map, marker);
     });
 
-    // Sets a listener on a radio button to change the filter type on Places
-    // Autocomplete.
     function setupClickListener(id, types) {
         var radioButton = document.getElementById(id);
         radioButton.addEventListener('click', function () {
@@ -97,81 +47,22 @@ function initMap() {
         });
 }
 
-
-function getWeatherJQ(lat, long) {
-    $.getJSON(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=0b078dc7f8ba04372a3c93f3eecba42c&units=metric`,
-        function (data) {
-            console.log(data);
-        }
-    );
-
-
-    //https://samples.openweathermap.org/data/2.5/weather?q=London,uk&callback=test&appid=0b078dc7f8ba04372a3c93f3eecba42c
-    /*$.ajax({
-        url: "http://api.openweathermap.org/data/2.5/weather",
-        jsonp: "callback",
-        dataType: "jsonp",
-        data: {
-            id: "2172797",
-            APPID: "35000cdad97645316c048563e4183021"
-        },
-        success: function( response ) {
-            console.log( response ); // server response
-            $('.current').html('<img src="http://openweathermap.org/img/w/' + response.weather[0].icon + '.png" /> ' + response.weather[0].main);
-        }
-    });*/
-}
-
 // get weather data according to the location
 function getWeather(lat, long) {
-    let result;
     const root = "https://api.openweathermap.org/data/2.5/forecast?appid=0b078dc7f8ba04372a3c93f3eecba42c&units=metric";
+
+
     fetch(`${root}&lat=${lat}&lon=${long}`, { method: "get" })
         .then(resp => resp.json())
         .then(data => {
-
             makeWeatherTable(weatherDataToArray(data));
-            /*
-            let weatherTable = arrayToTable(weatherDataToArray(data), {
-                thead: true,
-                attrs: {class: 'table'}
-            });
-            $('body').append(weatherTable);
-            */
-            //console.log(data);
-            //makeWeatherTable(data);
-            //console.log(weatherDataToArray(data));
         })
         .catch(function(err) {
             console.error(err);
         });
-    //return result;
 }
-/*
-var options = {
-    era: 'long',
-    year: 'numeric',
-    month: 'long',
-    day: 'numeric',
-    weekday: 'long',
-    timezone: 'UTC',
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric'
-};
-*/
 
 function weatherDataToArray(weatherData){
-    /*
-    console.log(weatherData.city.name);
-    console.log(weatherData.list[0].main.temp);
-    console.log(weatherData.list[0].weather[0].main);
-    console.log(weatherData.list[0].dt);
-    var date = new Date(+weatherData.list[0].dt*1000);
-    console.log(date.getMinutes().toString().length);
-    console.log(`${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes().toString().length<2?"0"+date.getMinutes():date.getMinutes()}`);
-    //console.log(date.toLocaleString("ru", options));
-    */
     let arr = [];
     let arrRow = [];
 
@@ -183,13 +74,10 @@ function weatherDataToArray(weatherData){
         arrRow = [];
     }
     return arr;
-    //console.log(getDateTimeStringFromUnixUTC(weatherData.list[0].dt));
 }
 
 function getDateTimeStringFromUnixUTC(dateUnixUTC) {
     let date = new Date(parseInt(dateUnixUTC)*1000);
-    //console.log(date.getMinutes().toString().length);
-    //console.log(`${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()} ${date.getHours()}:${date.getMinutes().toString().length<2?"0"+date.getMinutes():date.getMinutes()}`);
     let standardFormatTime = `${date.getHours()}:${date.getMinutes().toString().length<2?"0"+date.getMinutes().toString():date.getMinutes().toString()}`;
     let standardFormatDate = `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`
 
@@ -206,64 +94,10 @@ function makeWeatherTable(arr)
     });
 
     $('#pac-card').append(table);
-
-
 }
 
 
-
-
-$(document).ready(function(){
-
-    //getWeather(35,139);
-
-/*
-
-    var api = 'http://api.openweathermap.org/data/2.5/weather?id=2172797&APPID=35000cdad97645316c048563e4183021';
-        api = "https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=0b078dc7f8ba04372a3c93f3eecba42c";
-        //api = "http://api.openweathermap.org/data/2.5/weather?id=2172797&appid=0b078dc7f8ba04372a3c93f3eecba42c";
-    $.getJSON(api, {format:'json'},function(data){console.log(data.weather, "\n", data.main.temp)});
-*/
-
-/*
-    var data = [['Name', 'Age', 'Email'],['John Doe', '27', 'john@doe.com'],['Jane Doe', '29', 'jane@doe.com']];
-
-    var table = arrayToTable(data, {
-        thead: true,
-        attrs: {class: 'table'}
-    });
-
-    $('body').append(table);
-*/
-
-
-
-
-/*
-
-    weatherData = getWeather(35,139);
-    console.log(weatherData);
-    var weatherTable = arrayToTable(weatherDataToArray(weatherData), {
-        thead: true,
-        attrs: {class: 'table'}
-    });
-    $('body').append(weatherTable);*/
-
-});
-
-
-
-
-//BODY
-//getWeatherJQ(35,139);
-
-//console.log(getWeather2(35,139));
-//console.log(getWeather2);
-
-
-
 function arrayToTable(data, options) {
-
     "use strict";
 
     var table = $('<table />'),
@@ -321,6 +155,36 @@ function arrayToTable(data, options) {
     }
 
     return table;
-};
+}
 
 
+function httpGet(url) {
+
+    return new Promise(function(resolve, reject) {
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+
+        xhr.onload = function() {
+            if (this.status == 200) {
+                resolve(this.response);
+            } else {
+                var error = new Error(this.statusText);
+                error.code = this.status;
+                reject(error);
+            }
+        };
+
+        xhr.onerror = function() {
+            reject(new Error("Network Error"));
+        };
+
+        xhr.send();
+    });
+}
+
+httpGet("https://eugenebutovka.github.io/user.json")
+    .then(
+        response => console.log(`Fulfilled: ${response}`),
+        error => console.log(`Rejected: ${error}`)
+    );
